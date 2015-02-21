7a7f82b8e5d1d65730e7ca900959804905d9693f
lfmendivelso <lf.mendivelso10@uniandes.edu.co>
2015-02-21 14:12:20 -0500

    lf.mendivelso10 - HU - Emitir alarma a propietario - Modelo de datos.

--- alarms/tests.py
+++ alarms/tests.py
@@ -5,9 +5,12 @@ class StatusTestCase(TestCase):
     def loadCases(self):
         Status.objects.create(status='active')
         Status.objects.create(status='inactive')
+        Status.objects.create(status='unknown')
 
     def testStatus(self):
         act = Status.objects.get(status='active')
         dis = Status.objects.get(status='inactive')
+        unknown = Status.objects.get(status='unknown')
         self.assertEqual(act.status,'active')
         self.assertEqual(dis.status,'inactive')
+        self.assertEqual(unknown.status,'unknown')

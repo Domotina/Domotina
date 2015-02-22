f753c27d5ec23e286cca217bfc85f6bd93e67823
David Jimenez <d.jimenez13@uniandes.edu.co>
2015-02-22 17:07:51 -0500

    - Configurations to tart to use "Map App"

--- domotina/settings.py
+++ domotina/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'kronos',
     'alarms',
+    'map',
 )
 
 MIDDLEWARE_CLASSES = (
--- domotina/urls.py
+++ domotina/urls.py
@@ -9,11 +9,8 @@ router = routers.DefaultRouter()
 router.register(r'events', views.EventViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'domotina.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^map/', include('map.urls')),
 )

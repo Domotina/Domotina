76229054a974e995abc9cd3f573ae51d814b2390
cindy <cvho31@gmail.com>
2015-02-24 10:12:06 -0500

    Correccion Merge

--- domotina/urls.py
+++ domotina/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
 from event_manager import views
-
 from django.contrib import admin
+
 admin.autodiscover()
-
-# router = routers.DefaultRouter()
-# router.register(r'events', views.EventViewSet)
-
+router = routers.DefaultRouter()
+router.register(r'events', views.EventViewSet)
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^', include(router.urls)),
-    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^map/', include('map.urls')),
 )

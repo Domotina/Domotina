68dc81c50724fd27073c86dd0f9798191d529974
cindy <cvho31@gmail.com>
2015-02-24 10:06:21 -0500

    Correccion Merge

--- event_manager/views.py
+++ event_manager/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-
 # Create your views here.
-# from rest_framework import viewsets
-# from .models import Event
-# from .serializers import EventSerializer
-#
-# class EventViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = Event.objects.all()
-#     serializer_class = EventSerializer
+from rest_framework import viewsets
+from .models import Event, EventType
+from .serializers import EventSerializer, EventTypeSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventTypeViewSet(viewsets.ModelViewSet):
+    queryset = EventType.objects.all()
+    serializer_class = EventTypeSerializer

c1f55d90683a5b33719c5221a2783c2e196f0a9c
kaosterra <kaosterra@gmail.com>
2015-03-01 17:13:17 -0500

    Configuracion travis y vista de eventos

--- event_manager/views.py
+++ event_manager/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
-from .models import Event, EventType
+from .models import Event
 from .serializers import EventSerializer
 
 

5df5d6ae17d708467089b50e65bba07db53e761b
cindy <cvho31@gmail.com>
2015-03-08 15:15:17 -0500

    Login Funcionalidad de login

--- index/urls.py
+++ index/urls.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('index.views',
     url(r'^$', 'home', name='init'),
+    url(r'^map/', 'login', name='login'),
 )
--- index/views.py
+++ index/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            print("logueo")
+            redirect('map_home')
+        else:
+            print("no logueo")
+    else:
+        print("paila")

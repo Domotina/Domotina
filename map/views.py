b4f5d36813bc3c34abce70c67baccdd66c584b67
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-03-14 14:48:51 -0500

    Paginación Eventos Se incluye paginación al listado de eventos

--- map/views.py
+++ map/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from middleware.http import Http403
 from models import Place, Asset, Sensor, SensorStatus
-from django.conf import settings
 from event_manager.models import Event, Alarm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required
 def my_places(request):
@@ -37,9 +37,20 @@ def place_view(request, pk):
                 pass
 
     sensors_json = ','.join(sensors_array)
-    alarms = Alarm.objects.filter(event__sensor__asset__place=place).order_by('-activation_date')
-    events = Event.objects.filter(sensor__asset__place=place).order_by('-timestamp')
+    alarm_qs = Alarm.objects.filter(event__sensor__asset__place=place).order_by('-activation_date')
+    event_qs = Event.objects.filter(sensor__asset__place=place).order_by('-timestamp')
+
+    events_paginator = Paginator(event_qs, 5)
+    page = request.GET.get('page')
+    try:
+        events = events_paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        events = events_paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        events = events_paginator.page(events_paginator.num_pages)
 
     context = {'place': place, 'sensors': sensors_json,
-               'map_url': place.map, 'events': events, 'alarms': alarms}
+               'map_url': place.map, 'events': events, 'alarms': alarm_qs}
     return render(request, 'index_owner.html', context)

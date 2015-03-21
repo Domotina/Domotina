aba2e987f484e827895cb67a5a1e7b993cc86fb7
raulgomezn <raul.gomezn@hotmail.com>
2015-03-21 17:47:48 -0500

    Creacion de la autenticacion por grupos, salen 2 nuevos usuarios, central y user. Por favor borrar la base de datos!! y dar syncdb

--- central/views.py
+++ central/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
+def user_can_see(user):
+    return user.is_superuser or user.groups.filter(name='UsersCentral').exists()
 
+@login_required
+#@user_passes_test(user_can_see)
+#@permission_required('auth.change_logentry')
+@user_passes_test(user_can_see, login_url='/map/')
 def central_home(request):
-    return render_to_response('central_home.html')
+    context = {'user': request.user}
+    return render(request, 'central_home.html', context)
 
 def central_create(request):
-    return render_to_response('central_create.html')
+    context = {'user': request.user}
+    return render(request, 'central_create.html', context)
--- map/views.py
+++ map/views.py
@@ -4,8 +4,16 @@ from models import Place, Floor, Sensor, SensorStatus, SensorType
 from event_manager.models import Event, Alarm
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.decorators import user_passes_test
+
+
+def user_can_see(user):
+    return user.is_superuser or user.groups.filter(name='UsersOwners').exists()
 
 @login_required
+@user_passes_test(user_can_see, login_url='/central/')
 def my_places(request):
     places = Place.objects.filter(owner=request.user)
     context = {'user': request.user, 'places': places}

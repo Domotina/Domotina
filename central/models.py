ff8b310745dbb3042e4b6c809429ee2cbc86082a
raulgomezn <raul.gomezn@hotmail.com>
2015-03-21 09:25:56 -0500

    ajustes en la app central para separar el home de la central de la creacion masiva de inmubles.

--- central/models.py
+++ central/models.py
@@ -2,17 +2,3 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
-'''
-class Neighborhood(models.Model):
-    name = models.CharField("neighborhood", max_length=100)
-    date_created = models.DateTimeField("date created", auto_now_add=True)
-    date_updated = models.DateTimeField("date updated", auto_now_add=True)
-
-    class Meta:
-        verbose_name = "neighborhood"
-        verbose_name_plural = "neighborhoods"
-        ordering = ["name"]
-
-    def __unicode__(self):
-        return self.name
-'''
--- central/urls.py
+++ central/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('central.views',
     url(r'^$', 'central_home', name='central_home'),
-    url(r'(?P<pk>\d+)/$', 'central_create', name="central_create_view")
+    url(r'^create/', 'central_create', name="central_create")
 )

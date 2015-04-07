d182f3a072e16b77a5b2ec2db90c7379a1066af8
David Jimenez <d.jimenez13@uniandes.edu.co>
2015-04-07 15:00:58 -0500

    - Displaying the place name in the rule list to identify it

--- rule_engine/views.py
+++ rule_engine/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ScheduleDaily
+from map.models import Place
 # Create your views here.
 
 
 @login_required
 def list_rules(request, place):
+    place_obj = get_object_or_404(Place, pk=place)
     rules = ScheduleDaily.objects.filter(sensor__floor__place=place)
-    return render(request, 'list_rules.html', {'place_pk': place, 'rules': rules})
+    return render(request, 'list_rules.html', {'place': place_obj, 'rules': rules})
 
 
 @login_required
 def create_rule(request, place):
-    return render(request, 'create_rule.html', {'place_pk': place})
+    place_obj = get_object_or_404(Place, pk=place)
+    return render(request, 'create_rule.html', {'place': place_obj})
 
 
 @login_required
 def edit_rule(request, place, rule_pk):
+    place_obj = get_object_or_404(Place, pk=place)
     rule = get_object_or_404(ScheduleDaily, pk=rule_pk)
-    return render(request, 'create_rule.html', {'place_pk': place})
+    return render(request, 'create_rule.html', {'place': place_obj})
 
 
 @login_required

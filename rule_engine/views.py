9980c6ec0016ead28e9818844a675f183ef0b63e
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-04-07 18:12:08 -0500

    Formulario Reglas Se crea plantilla de formulario para creación y edición de reglas

--- rule_engine/views.py
+++ rule_engine/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ScheduleDaily
-from map.models import Place
+from map.models import Place, Sensor, SensorStatus
 # Create your views here.
 
 
@@ -14,15 +14,21 @@ def list_rules(request, place):
 
 @login_required
 def create_rule(request, place):
+    if request.method == 'POST':
+        print 'ES UN POST'
     place_obj = get_object_or_404(Place, pk=place)
-    return render(request, 'create_rule.html', {'place': place_obj})
+    sensors = Sensor.objects.filter(floor__place=place_obj)
+    statuses = SensorStatus.objects.all()
+    return render(request, 'create_rule.html', {'place': place_obj, 'sensors': sensors, 'statuses': statuses})
 
 
 @login_required
 def edit_rule(request, place, rule_pk):
     place_obj = get_object_or_404(Place, pk=place)
     rule = get_object_or_404(ScheduleDaily, pk=rule_pk)
-    return render(request, 'create_rule.html', {'place': place_obj})
+    sensors = Sensor.objects.filter(floor__place=place_obj)
+    statuses = SensorStatus.objects.all()
+    return render(request, 'create_rule.html', {'place': place_obj, 'sensors': sensors, 'statuses': statuses, 'rule': rule})
 
 
 @login_required

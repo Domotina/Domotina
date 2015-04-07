ed5a72a1ed9c5deb467dcbec93aed470f55e0bd1
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-04-07 18:55:56 -0500

    Creación y edición de reglas Funcionalidad para la creación y edición de reglas completada.

--- rule_engine/views.py
+++ rule_engine/views.py
@@ -15,7 +15,12 @@ def list_rules(request, place):
 @login_required
 def create_rule(request, place):
     if request.method == 'POST':
-        print 'ES UN POST'
+        if request.POST['sensor'] and request.POST['status'] and request.POST['begin_time'] and request.POST['end_time']:
+            sensor = Sensor.objects.get(pk=request.POST['sensor'])
+            status = SensorStatus.objects.get(pk=request.POST['status'])
+            rule = ScheduleDaily(sensor=sensor, status=status, begin_time=request.POST['begin_time'], end_time=request.POST['end_time'])
+            rule.save()
+            return redirect('list_rules', place=place)
     place_obj = get_object_or_404(Place, pk=place)
     sensors = Sensor.objects.filter(floor__place=place_obj)
     statuses = SensorStatus.objects.all()
@@ -26,6 +31,14 @@ def create_rule(request, place):
 def edit_rule(request, place, rule_pk):
     place_obj = get_object_or_404(Place, pk=place)
     rule = get_object_or_404(ScheduleDaily, pk=rule_pk)
+    if request.method == 'POST':
+        if request.POST['sensor'] and request.POST['status'] and request.POST['begin_time'] and request.POST['end_time']:
+            rule.sensor = Sensor.objects.get(pk=request.POST['sensor'])
+            rule.status = SensorStatus.objects.get(pk=request.POST['status'])
+            rule.begin_time = request.POST['begin_time']
+            rule.end_time = request.POST['end_time']
+            rule.save()
+            return redirect('list_rules', place=place)
     sensors = Sensor.objects.filter(floor__place=place_obj)
     statuses = SensorStatus.objects.all()
     return render(request, 'create_rule.html', {'place': place_obj, 'sensors': sensors, 'statuses': statuses, 'rule': rule})

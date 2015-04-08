5419aa27cd0f1ec674f2a0887b2dcb632ed32821
raulgomezn <raul.gomezn@hotmail.com>
2015-04-07 20:36:21 -0500

    Fix bug codacy

--- index/views.py
+++ index/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -14,9 +14,9 @@ def login(request):
     if user is not None:
         if user.is_active:
             login(request, user)
-            print("logueo")
+            print "logueo"
             redirect('map_home')
         else:
-            print("no logueo")
+            print "no logueo"
     else:
-        print("paila")
+        print "paila"

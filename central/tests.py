0d016548c89f3ad608eea11f354379d283d5746e
raulgomezn <raul.gomezn@hotmail.com>
2015-04-15 14:13:32 -0500

    Pruebas finales de HU delegados

--- central/tests.py
+++ central/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from map.models import Place
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import get_object_or_404
 # Create your tests here.
 
 class UserDelegate(unittest.TestCase):
@@ -72,6 +73,7 @@ class UserDelegate(unittest.TestCase):
             userTest.is_staff = False
             #userTest.groups.add()
             self.error_occured = False
+            userTest.delete()
         except:
             self.error_occured = True
 
@@ -106,7 +108,7 @@ class UserDelegate(unittest.TestCase):
     def testStage5(self):
         try:
              place = Place.objects.get(name='Salitre City') #inmueble
-             userTest = User.objects.create_user()
+             userTest = User.objects.create_user(username='')
              userTest.is_superuser = False
              userTest.is_active = True
              userTest.is_staff = False
@@ -123,40 +125,67 @@ class UserDelegate(unittest.TestCase):
         Cuando voy a configurar los permisos que va a tener un delegado en mi inmueble y seleccione el tipo de informacion que deseo revise
         Entonces se asignan los permisos de acceso seleccionados y se remueven los permisos no seleccionados a la informacion al usuario con rol delegado seleccionado.
         """
-        def testStage6(self):
-            userTest = User.objects.create_user(username=self.username, first_name=self.name,
-                                             last_name=self.lastName, email=self.email, password=self.password)
-            userTest.is_superuser = False
-            userTest.is_active = True
-            userTest.is_staff = False
-            #userTest.groups.add()
+    def testStage6(self):
+        userTest = User.objects.create_user(username=self.username, first_name=self.name,
+                                         last_name=self.lastName, email=self.email, password=self.password)
+        userTest.is_superuser = False
+        userTest.is_active = True
+        userTest.is_staff = False
+        #userTest.groups.add()
 
-            content_type = ContentType.objects.get_for_model(Place)
-            permission = Permission.objects.get(content_type=content_type, codename='is_member')
+        content_type = ContentType.objects.get_for_model(Place)
+        permission = Permission.objects.get(content_type=content_type, codename='add_place')
 
-            userTest.user_permissions.add(permission)
-
-            self.assertEqual(userTest.username, self.username)
-            self.assertEqual(userTest.first_name, self.name)
-            self.assertEqual(userTest.last_name, self.lastName)
-            self.assertEqual(userTest.email, self.email)
-            self.assertEqual(userTest.user_permissions, permission)
-            userTest.delete() #No borrar
+        userTest.user_permissions.add(permission)
+        userTest.save()
+        self.assertEqual(userTest.username, self.username)
+        self.assertEqual(userTest.first_name, self.name)
+        self.assertEqual(userTest.last_name, self.lastName)
+        self.assertEqual(userTest.email, self.email)
+        # Request new instance of User
+        user = get_object_or_404(User, pk=userTest.pk)
+        self.assertTrue(user.has_perm('map.add_place') == True)
+        userTest.delete() #No borrar
         """
         Escenario 7
         Dado que soy un propietario
         Cuando voy a configurar los permisos que va a tener un delegado en mi inmueble y no selecciono el delegado
         Entonces no se asignan los permisos y se solicita seleccionar un delegado.
-"""
+        """
+    def testStage7(self):
+        try:
+            user = User.objects.get(username='')
+            content_type = ContentType.objects.get_for_model(Place)
+            permission = Permission.objects.get(content_type=content_type, codename='add_place')
+            #user.groups.add()
+            place = Place.objects.get(name='Salitre City') #inmueble
+            self.error_occured = False
+        except:
+            self.error_occured = True
 
+        self.assertTrue(self.error_occured == True)
         """
         Escenario 8
         Dado que soy un delegado
         Cuando ingreso por URL, una direccion a la cual no tengo permisos
         Entonces no se debe mostrar la informacion de esta URL ingresada.
+        """
+    def testStage8(self):
+        user = User.objects.get(username='buildersmart')
+        content_type = ContentType.objects.get_for_model(Place)
+        permission = Permission.objects.get(content_type=content_type, codename='add_place')
 
+        self.assertTrue(user.has_perm('map.add_place') == False)
+
+        """
         Escenario 9
         Dado que soy un delegado
         Cuando ingreso por URL, una direccion a la cual tengo permisos
         Entonces se debe mostrar la informacion de esta URL ingresada.
         """
+    def testStage9(self):
+        user = User.objects.get(username='domotina')
+        content_type = ContentType.objects.get_for_model(Place)
+        permission = Permission.objects.get(content_type=content_type, codename='add_place')
+
+        self.assertTrue(user.has_perm('map.add_place') == True)

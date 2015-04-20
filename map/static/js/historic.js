fb2d14afafa841547f8b17e89758bebbd92ab311
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-04-19 19:10:01 -0500

    Histórico de eventos Cuando se selecciona una fecha en el calendario, y se hace clic en Go (o se presiona enter), se redirige al histórico de la fecha seleccionada.

--- map/static/js/historic.js
+++ map/static/js/historic.js
@@ -15,9 +15,22 @@ function secondsFormatter(value) {
 };
 
 $(function () {
+    /*
+    Se carga el mapa del primer piso por defecto
+     */
+    $('#map').css("background-image", "url(" + floor.url + ")");
+
+    /*
+    Se registran todas las opciones de pisos una vez carga el documento
+     */
     for (idx in floors) {
         $("#floors").append("<option value=" + idx + ">Floor " + floors[idx].number + "</option>");
     }
+
+    /*
+    Cuando se selecciona un piso, se pinta de nuevo el mapa del piso
+    y los respectivos sensores
+     */
     $("#floors").change(function(){
         var idx = $("#floors").find(":selected").val();
         floor = floors[idx];
@@ -25,8 +38,9 @@ $(function () {
         showIcons();
     });
 
-    $('#map').css("background-image", "url(" + floor.url + ")");
-
+    /*
+    Se crea el slider y se añade comportamiento cuando cambia su valor
+     */
     var slider = $('#slider').slider({formater: secondsFormatter})
     slider.on('slide', function (value) {
         var seconds = value.value;
@@ -35,4 +49,16 @@ $(function () {
         time.setSeconds(seconds % 60);
         showIcons()
     });
+
+    /*
+    Cuando se selecciona una fecha, se navega hasta el histórico en dicha fecha
+     */
+    $("#goToDate").submit(function(event){
+        var value = $("#calendar").val();
+        if(value){
+            var url = window.location.pathname.replace(/\d+\/$/, value.replace(/-/g,"")+"/");
+            window.location.pathname = url;
+        }
+        event.preventDefault()
+    })
 });
